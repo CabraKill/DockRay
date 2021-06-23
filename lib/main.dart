ca1cd29acb2b3dc9dc65fb0dc018eb0d353b8fc2
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-06-23 18:03:10 -0300

    Changes to GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(GetMaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
   ));
