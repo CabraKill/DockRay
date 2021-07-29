e5be01426462e7a0ebe4e355e50ff44e9a74101c
raphael-linux <raphaeldesouza@outlook.com>
2021-07-29 17:58:34 -0300

    Update main with getx structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:system_call_test/pages/pages.dart';
 
-import 'pages/home.dart';
+import 'initialBinding.dart';
 
 void main() {
   runApp(GetMaterialApp(
-    home: HomePage(),
+    getPages: Pages.pages,
+    initialRoute: "/home",
+    initialBinding: InitialBinding(),
     debugShowCheckedModeBanner: false,
   ));
 }
