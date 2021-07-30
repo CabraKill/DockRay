c7c988662177399a55532928ae8b0149323ee99a
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-07-30 08:25:13 -0300

    Remove unused import from showLoadingDialog

--- lib/utils/dialogs/loadingDialog/showLoadingDialog.dart
+++ lib/utils/dialogs/loadingDialog/showLoadingDialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:system_call_test/utils/dialogs/loadingDialog/loadingDialog.dart';
 
 Future<void> showLoadingDialog(context) async {
