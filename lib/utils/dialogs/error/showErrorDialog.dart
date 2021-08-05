53d62beb8815f5ec4ef0938ee193e8f0490fe03c
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-05 12:58:32 -0300

    Add error printing

--- lib/utils/dialogs/error/showErrorDialog.dart
+++ lib/utils/dialogs/error/showErrorDialog.dart
@@ -1,5 +1,7 @@
 import 'package:dockray/utils/dialogs/error/errorDialog.dart';
 import 'package:get/get.dart';
 
-Future<void> showErrorDialog(String error) =>
-    Get.dialog(ErrorDialog(error: error));
+Future<void> showErrorDialog(String error) {
+  print(error);
+  return Get.dialog(ErrorDialog(error: error));
+}
