4834efa5380ca2513a7e20aa98a438903b4f8ea9
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-18 16:03:45 -0300

    Add splash to pages

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,9 +1,12 @@
+import 'package:dockray/pages/splash/splashBinding.dart';
+import 'package:dockray/pages/splash/splashPage.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'home/home.dart';
 import 'home/homeBinding.dart';
 
 class Pages {
   static final pages = [
+    GetPage(name: '/', page: () => SplashPage(), binding: SplashBinding()),
     GetPage(name: "/home", page: () => HomePage(), binding: HomeBinding())
   ];
 }
