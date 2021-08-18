a64336071d9b006f4c8487ab61b08451fa7cefca
Raphael-pc-linux <raphael.pereira@grupofasitec.com.br>
2021-08-18 16:07:10 -0300

    Create docker container driver implementation with IO

---
+++ lib/docker/data/IO/dockerContainersIODriver.dart
@@ -0,0 +1,9 @@
+import '../../containers/IdockerContainersDriver.dart';
+
+class DockerContainersIODriver implements IDockerContainersDriver {
+  @override
+  getContainers() {
+    // TODO: implement getContainers
+    throw UnimplementedError();
+  }
+}
